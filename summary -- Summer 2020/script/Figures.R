b797187d9527cdc39b1e1f579f17c4dfd6d0246b
Han <yinhan@csu.fullerton.edu>
2020-06-23 11:32:27 -0700

    F5 update

--- summary -- Summer 2020/script/Figures.R
+++ summary -- Summer 2020/script/Figures.R
@@ -374,46 +374,31 @@ labels = read.csv("./data/DIET 1 raw.csv") %>%
   group_by(Diet,Treatment,Sex) %>% 
   summarise(Initial=sum(Initial.density))
 
-plot_fun5 = function(d){
-  set_label = 
-    labels %>% 
-    filter(Diet==d) %>% 
-    mutate(Sex = ifelse(Sex=="M", "Male","Female"),
-           Sex = factor(Sex, levels=c("Male","Female")),
-           tag = paste0(Treatment," ",Sex, " (n=",Initial,")")) %>% 
-    arrange(Treatment,Sex)
-  
-  bind_rows(df,df_initial) %>% 
-    filter(Diet == d) %>% 
+
+bind_rows(df) %>% 
+    group_by(Diet) %>% 
     mutate(tag = factor(paste(Treatment, Sex), 
                         levels=c("Control Male","Control Female",
                                  "Fungal Male","Fungal Female"))) %>% 
-    ggplot(aes(x=day, y=est, ymin=lower, ymax=upper, color=tag, linetype=tag,
+    ggplot(aes(x=day, y=est, ymin=lower, ymax=upper, color=tag, 
                group=interaction(Diet,tag))) +
-    geom_errorbar(width=0.5,size=0.5,alpha=0.7) +
-    geom_line(size=1) +
+    geom_errorbar(width=0.7,size=0.6,alpha=0.7) +
+    geom_line(size=0.8, aes(linetype=tag)) +
     geom_point(size=1) +
     scale_x_continuous(breaks=seq(0,12,4)) +
     scale_linetype_manual(values=c("Control Female"="dotted",
                                    "Control Male"="dotted",
                                    "Fungal Female"="solid",
-                                   "Fungal Male"="solid"),
-                          labels=set_label$tag) +
+                                   "Fungal Male"="solid")) +
     scale_color_manual(values=c("Control Female"="darkgreen",
                                 "Control Male"="brown",
                                 "Fungal Female"="darkgreen",
-                                "Fungal Male"="brown"),
-                       labels=set_label$tag) +
+                                "Fungal Male"="brown")) +
     facet_grid(~Diet) +
     labs(x="Days after spray", y="Survival Probability", color="", linetype="") +
     theme_classic()
-}
+    
 
-plot_fun5("C/C")
-plot_fun5("C/CY")
-plot_fun5("CY/C")
-plot_fun5("CY/CY")
-plot_fun5("G/G")
 
 
 ### --------------------------    F6    --------------------------  
